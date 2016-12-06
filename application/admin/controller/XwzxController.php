75400e76e1e592d4603cba67ca904bd88083d24a
holyland2016 <tengshengdi@sina.com>
2016-12-06 21:46:53 +0800

    新闻中心

---
+++ application/admin/controller/XwzxController.php
@@ -0,0 +1,290 @@
+<?php
+namespace app\admin\controller;
+use app\admin\model\User;
+use app\admin\model\Xwzx as XwzxModel;
+use think\Controller;
+use app\admin\controller\AdminAuthController;
+use think\Validate;
+/**
+ *
+ */
+class XwzxController extends AdminAuthController
+{
+  //模块基本信息
+  private $data = array(
+    // 'upload_path' => UPLOAD_PATH,
+    'module_url'  => '/admin/xwzx/',
+    'upload_url'  => '/public/uploads/',
+      'ckeditor'    => array(
+            'id'     => 'ckeditor_post_content',
+            //Optionnal values
+            'config' => array(
+                'width'  => "100%", //Setting a custom width
+                'height' => '400px',
+            )
+        ),
+  );
+  // 创建新文章
+  public function create()
+  {
+    $admins = User::where('status',1)->column('username','nickname');
+    // dump($list);
+    $this->data['edit_fields'] = array(
+      'post_title'     => array('type' => 'text', 'placeholder' => '在此输入标题'),
+      'post_content'   => array('type' => 'textarea', 'id'=>'ckeditor_post_content'),
+      'post_author'    => array('type' => 'select', 'label' => '作者','default' => $admins, 'extra'=>array('wrapper'=>'col-sm-3')),
+      'status'         => array('type' => 'radio', 'label' => '状态','default'=> array(-1 => '删除', 0 => '草稿', 1 => '发布',2 => '待审核')),
+      'taxonomy'       => array('type' => 'taxonomy', 'label' => '分类','default'=>
+                          array( 1=>'图片新闻',2=>'综合新闻',3=>'科研动态',4=>'学术活动',5=>'通知公告')),
+      'create_time'    => array('type' => 'text1', 'label' => '发布时间','id'=>'datetimepicker1','extra'=>array('format'=>'YYYY-MM-DD hh:mm:ss')),
+      'feature_image'  => array('type' => 'file','label'     => '特色图片','exampleInputFile'		=> '点击图片上传'),
+        // 'post_excerpt'   => array('type' => 'textarea', 'label' => '摘要'),
+    );
+
+    //默认值设置
+    $item['status']         = '发布';
+    $item['comment_status'] = config('comment_toggle') ? '打开' : '关闭';
+    $item['create_time']    = date('Y-m-d H:i:s');
+
+    $this->assign('item',$item);
+    $this->assign('data',$this->data);
+    return $this->fetch('',[],['__PUBLIC__'=>'/static']);
+  }
+
+  public function read($id='')
+    {
+        $admins = user::where('status',1)->column('nickname','id');
+
+        $this->data['edit_fields'] = array(
+					'post_title'     => array('type' => 'text', 'placeholder' => '在此输入标题'),
+			  	'post_content'   => array('type' => 'textarea', 'id'=>'ckeditor_post_content'),
+					'post_author'    => array('type' => 'select', 'label' => '作者','default' => $admins, 'extra'=>array('wrapper'=>'col-sm-3')),
+					'status'         => array('type' => 'radio', 'label' => '状态','default'=> array(-1 => '删除', 0 => '草稿', 1 => '发布',2 => '待审核')),
+          'taxonomy'       => array('type' => 'taxonomy', 'label' => '分类','default'=>
+                              array( 1=>'图片新闻',2=>'综合新闻',3=>'科研动态',4=>'学术活动',5=>'通知公告')),
+          'create_time'    => array('type' => 'text1', 'label' => '发布时间','id'=>'datetimepicker1','extra'=>array('format'=>'YYYY-MM-DD hh:mm:ss')),
+					'feature_image'  => array('type' => 'file','label'     => '特色图片','exampleInputFile'		=> '点击图片上传'),
+        );
+
+        //默认值设置
+        $item = XwzxModel::get($id);
+        $item['post_content'] = str_replace('&', '&amp;', $item['post_content']);
+
+        $this->assign('item',$item);
+        $this->assign('data',$this->data);
+
+        return $this->fetch('',[],['__PUBLIC__'=>'/static']);
+    }
+
+  public function add()
+  {
+    $posts = new XwzxModel;
+    $data = input('post.');
+    $rule = [
+    'post_title|文章标题' => 'require',
+  ];
+    // $data['feature_image'] = $this->upload();
+    // 	if(!$data['feature_image']){
+    // 		unset($data['feature_image']);
+    // 	}
+
+    $data['create_time'] = $data['create_time'] ? strtotime($data['create_time']) : time();
+    $data['update_time'] = time();
+    if ($posts->insertGetId($data)) {
+      $this->redirect('admin/xwzx/index');
+    } else {
+      return $posts->getError();
+    }
+    // return $this->fetch('',[],['__PUBLIC__'=>'/static']);
+  }
+
+
+  public function index()
+  {
+		$request = request();
+	  $param = $request->param();
+    $map['status'] = ['>=','0'];
+
+		if(!empty($param)){
+			$this->data['search'] = $param;
+			if(isset($param['title'])){
+				$map['post_title'] = ['like','%'.$param['title'].'%'];
+			}
+			if(isset($param['start_time'])){
+						$map['create_time'] = ['>= time',$param['start_time']];
+			}
+			// 结束日期还未书写
+		}
+		// $map['create_time'] = ['between time',['2016-11-18','	2016-11-20']];
+		$list =  XwzxModel::where($map)
+	          ->order('create_time', 'DESC')
+	          ->paginate();
+
+		$this->assign('data',$this->data);
+    $this->assign('list',$list);
+
+		return $this->fetch('',[],['__PUBLIC__'=>'/static']);
+  }
+
+  public function tpxw()
+  {
+    $request = request();
+    $param = $request->param();
+    $map['status'] = ['>=','0'];
+    $map['taxonomy'] = ['=','1'];
+
+    if(!empty($param)){
+      $this->data['search'] = $param;
+      if(isset($param['title'])){
+        $map['post_title'] = ['like','%'.$param['title'].'%'];
+      }
+      if(isset($param['start_time'])){
+            $map['create_time'] = ['>= time',$param['start_time']];
+      }
+      // 结束日期还未书写
+    }
+    // $map['create_time'] = ['between time',['2016-11-18','	2016-11-20']];
+    $list =  XwzxModel::where($map)
+            ->order('create_time', 'DESC')
+            ->paginate();
+
+    $this->assign('data',$this->data);
+    $this->assign('list',$list);
+
+    return $this->fetch('',[],['__PUBLIC__'=>'/static']);
+  }
+  // comprehensive
+  public function zhxw()
+  {
+    $request = request();
+    $param = $request->param();
+    $map['status'] = ['>=','0'];
+    $map['taxonomy'] = ['=','2'];
+
+    if(!empty($param)){
+      $this->data['search'] = $param;
+      if(isset($param['title'])){
+        $map['post_title'] = ['like','%'.$param['title'].'%'];
+      }
+      if(isset($param['start_time'])){
+            $map['create_time'] = ['>= time',$param['start_time']];
+      }
+      // 结束日期还未书写
+    }
+    // $map['create_time'] = ['between time',['2016-11-18','	2016-11-20']];
+    $list =  XwzxModel::where($map)
+            ->order('create_time', 'DESC')
+            ->paginate();
+
+    $this->assign('data',$this->data);
+    $this->assign('list',$list);
+
+    return $this->fetch('',[],['__PUBLIC__'=>'/static']);
+  }
+
+  public function kytd()
+  {
+    $request = request();
+    $param = $request->param();
+    $map['status'] = ['>=','0'];
+    $map['taxonomy'] = ['=','3'];
+
+    if(!empty($param)){
+      $this->data['search'] = $param;
+      if(isset($param['title'])){
+        $map['post_title'] = ['like','%'.$param['title'].'%'];
+      }
+      if(isset($param['start_time'])){
+            $map['create_time'] = ['>= time',$param['start_time']];
+      }
+      // 结束日期还未书写
+    }
+    // $map['create_time'] = ['between time',['2016-11-18','	2016-11-20']];
+    $list =  XwzxModel::where($map)
+            ->order('create_time', 'DESC')
+            ->paginate();
+
+    $this->assign('data',$this->data);
+    $this->assign('list',$list);
+
+    return $this->fetch('',[],['__PUBLIC__'=>'/static']);
+  }
+
+  public function xshd()
+  {
+    $request = request();
+    $param = $request->param();
+    $map['status'] = ['>=','0'];
+    $map['taxonomy'] = ['=','4'];
+
+    if(!empty($param)){
+      $this->data['search'] = $param;
+      if(isset($param['title'])){
+        $map['post_title'] = ['like','%'.$param['title'].'%'];
+      }
+      if(isset($param['start_time'])){
+            $map['create_time'] = ['>= time',$param['start_time']];
+      }
+      // 结束日期还未书写
+    }
+    // $map['create_time'] = ['between time',['2016-11-18','	2016-11-20']];
+    $list =  XwzxModel::where($map)
+            ->order('create_time', 'DESC')
+            ->paginate();
+
+    $this->assign('data',$this->data);
+    $this->assign('list',$list);
+
+    return $this->fetch('',[],['__PUBLIC__'=>'/static']);
+  }
+
+  public function tzgg()
+  {
+    $request = request();
+    $param = $request->param();
+    $map['status'] = ['>=','0'];
+    $map['taxonomy'] = ['=','5'];
+
+    if(!empty($param)){
+      $this->data['search'] = $param;
+      if(isset($param['title'])){
+        $map['post_title'] = ['like','%'.$param['title'].'%'];
+      }
+      if(isset($param['start_time'])){
+            $map['create_time'] = ['>= time',$param['start_time']];
+      }
+      // 结束日期还未书写
+    }
+    // $map['create_time'] = ['between time',['2016-11-18','	2016-11-20']];
+    $list =  XwzxModel::where($map)
+            ->order('create_time', 'DESC')
+            ->paginate();
+
+    $this->assign('data',$this->data);
+    $this->assign('list',$list);
+
+    return $this->fetch('',[],['__PUBLIC__'=>'/static']);
+  }
+
+  public function update($id='')
+  {
+      $posts = new XwzxModel;
+      $data = input('post.');
+      $data['id'] = $id;
+      if ($posts->update($data)) {
+          return $this->success('信息更新成功','/admin/xwzx/index');
+      } else {
+          return $posts->getError();
+      }
+  }
+  public function delete($id='')
+	{
+		$result = XwzxModel::destroy($id);
+    if($result){
+      $this->redirect('admin/xwzx/index');
+    }else{
+      return $result->getError();
+    }
+	}
+
+}
--- application/index/controller/XwzxController.php
+++ application/index/controller/XwzxController.php
@@ -6,52 +6,79 @@ use think\Db;
 
 use app\admin\model\Xwzx as XwzxModel;
 
-class IndexController extends Controller
+class XwzxController extends Controller
 {
     public function index()
     {
-      $map1['status'] = ['>=','0'];
-      $map1['taxonomy'] = ['=','1'];
-      $tpxw =  XwzxModel::where($map1)
-              ->order('create_time', 'DESC')
-              ->paginate();
-      $map2['status'] = ['>=','0'];
-      $map2['taxonomy'] = ['=','2'];
-      $zhxw =  XwzxModel::where($map2)
-              ->order('create_time', 'DESC')
-              ->paginate();
-      $map3['status'] = ['>=','0'];
-      $map3['taxonomy'] = ['=','3'];
-      $kydt =  XwzxModel::where($map3)
-              ->order('create_time', 'DESC')
-              ->paginate();
-      // $this->assign('data',$this->data);
-      $map4['status'] = ['>=','0'];
-      $map4['taxonomy'] = ['=','4'];
-      $xshd =  XwzxModel::where($map4)
-              ->order('create_time', 'DESC')
-              ->paginate();
-      $map5['status'] = ['>=','0'];
-      $map5['taxonomy'] = ['=','3'];
-      $tzgg =  XwzxModel::where($map5)
-              ->order('create_time', 'DESC')
-              ->paginate();
+
       $map['status'] = ['>=','0'];
               // $map1['taxonomy'] = ['=','1'];
       $all =  XwzxModel::where($map)
               ->order('create_time', 'DESC')
               ->paginate();
 
-      $this->assign('tpxw',$tpxw);
-      $this->assign('zhxw',$zhxw);
-      $this->assign('kydt',$kydt);
-      $this->assign('xshd',$xshd);
-      $this->assign('tzgg',$tzgg);
-
       $this->assign('all',$all);
 
       return $this->fetch();
       // echo $id;
+    }
+
+    public function tpxw()
+    {
+      $map1['status'] = ['>=','0'];
+      $map1['taxonomy'] = ['=','1'];
+      $tpxw =  XwzxModel::where($map1)
+              ->order('create_time', 'DESC')
+              ->paginate();
+
+
+      // $this->assign('data',$this->data);
+      $this->assign('tpxw',$tpxw);
+
+      return $this->fetch();
+      // echo $id;
+    }
+    public function zhxw()
+    {
+      $map2['status'] = ['>=','0'];
+      $map2['taxonomy'] = ['=','2'];
+      $zhxw =  XwzxModel::where($map2)
+              ->order('create_time', 'DESC')
+              ->paginate();
+     $this->assign('zhxw',$zhxw);
+     return $this->fetch();
+
+    }
+    public function kydt()
+    {
+      $map3['status'] = ['>=','0'];
+      $map3['taxonomy'] = ['=','3'];
+      $kydt =  XwzxModel::where($map3)
+              ->order('create_time', 'DESC')
+              ->paginate();
+      $this->assign('kydt',$kydt);
+      return $this->fetch();
+    }
+    public function xshd()
+    {
+      $map4['status'] = ['>=','0'];
+      $map4['taxonomy'] = ['=','4'];
+      $xshd =  XwzxModel::where($map4)
+              ->order('create_time', 'DESC')
+              ->paginate();
+      $this->assign('xshd',$xshd);
+      return $this->fetch();
+    }
+    public function tzgg()
+    {
+
+      $map5['status'] = ['>=','0'];
+      $map5['taxonomy'] = ['=','3'];
+      $tzgg =  XwzxModel::where($map5)
+            ->order('create_time', 'DESC')
+            ->paginate();
+      $this->assign('tzgg',$tzgg);
+      return $this->fetch();
     }
 
     public function read($id='')
